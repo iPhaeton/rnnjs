7e056d074f3f4206b3941d6126223fd70f4b2e57
Ilya Bohaslauchyk <bohaslauchyk@gmail.com>
2019-01-13 13:14:32 +0300

    Initialize weight matrices woth small numbers

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
+const math = require('mathjs')
 const {readText} = require('./utils');
 const FILENAME = 'input.txt';
 const EOS = '\n';
 
-async function rnn() {
-  const [inputs, vocabulary, charToIndex] = await readText(FILENAME, EOS)
+function initialize(vocabSize, hiddenSize) {
+  const wxh = math.multiply(math.ones(hiddenSize, vocabSize), math.random() * 0.01);
+  const whh = math.multiply(math.ones(hiddenSize, hiddenSize), math.random() * 0.01);
+  const why = math.multiply(math.ones(vocabSize, hiddenSize), math.random() * 0.01);
+
+  return [wxh, whh, why];
 }
 
-rnn()
+async function rnn(hiddenSize) {
+  const [inputs, vocabulary, charToIndex] = await readText(FILENAME, EOS);
+  const [wxh, whh, why] = initialize(vocabulary.length, hiddenSize);
+}
+
+rnn(100);
