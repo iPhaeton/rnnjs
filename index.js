bb074cd60c9ff8c2d2d74c029c7d4a34edd8838c
Ilya Bohaslauchyk <bohaslauchyk@gmail.com>
2019-01-14 12:34:56 +0300

    Implement stepForward function

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const {readText} = require('./utils');
 const FILENAME = 'input.txt';
 const EOS = '\n';
 
-function initialize(vocabSize, hiddenSize) {
-  const w = math.multiply(math.ones(hiddenSize, vocabSize + hiddenSize), math.random() * 0.01);
-  const why = math.multiply(math.ones(vocabSize, hiddenSize), math.random() * 0.01);
+function initialize([vocabSize, hiddenSize]) {
+  const w = math.multiply(math.random([hiddenSize, vocabSize + hiddenSize]), 0.01);
+  const why = math.multiply(math.random([vocabSize, hiddenSize]), 0.01);
+  const bh = math.zeros(hiddenSize, 1);
+  const by = math.zeros(vocabSize, 1);
 
-  return [w, why];
+  return [w, why, bh, by];
 }
 
 function oneHotEncode(sequence, vocabSize, charToIndex) {
@@ -18,12 +20,28 @@ function oneHotEncode(sequence, vocabSize, charToIndex) {
   })
 }
 
+function stepForward(oneHotChar, oneHotY, [vocabSize, hiddenSize], [w, why, bh, by]) {
+  hPrev = math.zeros(1, hiddenSize);
+  const xh = math.concat(oneHotChar, hPrev);
+
+  let h = math.multiply(w, math.reshape(xh, [123,1]));
+  h = math.add(h, bh);
+  h = math.tanh(h);
+  let scores = math.multiply(why, h);
+  scores = math.add(scores, by);
+
+  return [h, scores];
+}
+
 async function rnn(hiddenSize) {
   const [inputs, vocabulary, charToIndex] = await readText(FILENAME, EOS);
-  const [w, why] = initialize(vocabulary.length, hiddenSize);
-  const sequences = inputs.map(seq => oneHotEncode(seq, vocabulary.length, charToIndex));
+  const sizes = [vocabulary.length, hiddenSize];
+  const weights = initialize(sizes);
+  const sequences = inputs.map(seq => oneHotEncode(seq, sizes[0], charToIndex));
 
-  console.log(sequences[0][1])
+  const [h, scores] = stepForward(sequences[0][0], sequences[0][1], sizes, weights);
+  console.log(h);
+  //sequences.forEach(seq => forwardPass(seq, sizes, weights))
 }
 
 rnn(100);
